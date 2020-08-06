ba4cfab196277e884085e106b88f24b166a12947
Christian Miller <cnmiller127@gmail.com>
2020-08-05 21:32:30 -0500

    server env port

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const logger = require("morgan");
 const Workouts = require("./models/workouts");
 const seed = require("./seeders/seed");
+var PORT = process.env.PORT || 3007;
 
 const app = express();
 
@@ -13,7 +14,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongodb://heroku_12gbh048 : 7tmm24mfa5phd385b8a55457uj @ds143231.mlab.com: 43231/heroku_12gbh048
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
 //Toggle on to reset database with seed file
@@ -21,6 +21,6 @@ require("./routes/html-routes")(app);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://cnmiller127:superBase93!@ds227322.mlab.com:27322/heroku_51b9phxn", { useNewUrlParser: true });
 
 // Listen on port 3000
-app.listen(3000, () => {
-  console.log("App running on port 3000! " + "http://localhost:3000/");
+app.listen(PORT, () => {
+  console.log("App running!");
 });
