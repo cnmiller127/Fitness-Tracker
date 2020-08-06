102d98cae2f909c74fc6d1e524466707bf5c7e9b
Christian Miller <cnmiller127@gmail.com>
2020-08-05 21:28:02 -0500

    mLab setup

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-
+mongodb://heroku_12gbh048 : 7tmm24mfa5phd385b8a55457uj @ds143231.mlab.com: 43231/heroku_12gbh048
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
 //Toggle on to reset database with seed file
 // seed();
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://cnmiller127:superBase93!@ds227322.mlab.com:27322/heroku_51b9phxn", { useNewUrlParser: true });
 
 // Listen on port 3000
 app.listen(3000, () => {
